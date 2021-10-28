d410e409736fe4e9031f7065a31d31190e541f96
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2021-10-28 12:13:15 +0530

    Create lengthOfLastWord.cpp

---
+++ Beginner/lengthOfLastWord.cpp
@@ -0,0 +1,13 @@
+ class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0,i;
+        s=s.trim();
+        for(i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)==' ')
+                break;
+                ++count;
+        }
+            return count;
+    }
+}
